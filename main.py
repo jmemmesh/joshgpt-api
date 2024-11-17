210b59130d2c73509a3de894ed83bc6187462b03
jmemmesh <jmemmes@gmail.com>
2024-11-16 19:29:52 -0500

    hello world

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-  pass
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_answer():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-  main()
+    app.run(debug=True)
